22a522ce7f548000565c3fa8350a828ec8a9c862
Raffi Joeta <raffijatirogo@gmail.com>
2024-02-22 00:51:25 +0700

    penambahan index

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require(`express`);
 const app = express();
-const PORT = 2000;
+const PORT = 8000;
 const cors = require(`cors`);
+require('dotenv').config();
 app.use(cors());
 // const bodyParser = require('body-parser')
 // app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,13 +13,19 @@ const siswaRoute = require(`./routes/siswa.routes`)
 app.use(`/siswa`, siswaRoute);
 
 const guruRoute = require(`./routes/guru.routes`)
-app.use(`/guru`, guruRoute)
+app.use(`/guru`, guruRoute);
 
 const transaksiSiswaRoute = require('./routes/transaksiSiswa_routes')
 app.use(`/transaksi_siswa`, transaksiSiswaRoute);
 
 const transaksiKurirRoute = require('./routes/transaksiKurir_routes')
 app.use(`/transaksi_kurir`, transaksiKurirRoute);
+
+const adminRoute = require('./routes/admin.routes')
+app.use('/admin', adminRoute);
+
+const authRoute = require('./routes/auth.routes')
+app.use('/api', authRoute);
 
 
 app.listen(PORT, () => {
