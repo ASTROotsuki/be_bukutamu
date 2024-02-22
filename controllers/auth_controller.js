e548abff79df64fddb01f5edaf44e650c98aa2ae
Raffi Joeta <raffijatirogo@gmail.com>
2024-02-22 09:46:15 +0700

    pembaruan login

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -8,37 +8,37 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const generateToken = (userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '12' });
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '12h' });
     return token;
 };
 
 exports.login = async (request, response) => {
-    let { email, password } = request.body
+    if (request.body) {
+        let { email, password } = request.body;
 
-    try{
-        const user = await adminModel.findOne({ where: { email } })
-
-        if (!user) {
-            return response.status(401).json({
-                success: 'false', logged: 'false', message: 'Invalid Email'
-            });
+        try {
+            const user = await adminModel.findOne({ where: { email } });
+            
+            if (!user) {
+                return response.status(401).json({success: 'false', logged: 'false', message: 'Invalid Email' });
+            }
+    
+            const credential =  await bcrypt.compare(password, user.password);
+    
+            if (!credential) {
+                return response.status(401).json({success: 'false', logged: 'false', message: 'Invalid Password' });
+            }
+    
+            const token = generateToken(user.uuid);
+    
+            return response.json({ success: 'true', logged: 'true', nama: user.nama_admin, token: token });
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ success: false, message: "Internal Server Error" });
         }
-
-        const credential = await bcrypt.compare(password, user.password)
-
-        if (!credential) {
-            return response.status(401).json({
-                success: 'false', logged: 'false', message: 'Invalid Password'
-            });
-        }
-
-        const token = generateToken(user.uuid);
-
-        return response.json({
-            success: 'true', logged: 'true', nama: user.nama_admin, token: token
-        })
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({ success: false, message: 'Internal Server Error'});
+    } else {
+        response.status(400).json({ error: 'Bad request' });
     }
+
+    
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/auth_controller');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // endpoint
 app.post('/login', authController.login)
