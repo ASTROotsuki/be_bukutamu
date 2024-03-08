3164c4017bff159186455712bc511fdaa928fd0c
Raffi Joeta <raffijatirogo@gmail.com>
2024-03-08 16:57:11 +0700

    kurir verif

--- routes/transaksiKurir_routes.js
+++ routes/transaksiKurir_routes.js
@@ -3,19 +3,17 @@ const router = express()
 const cron = require('node-cron')
 const nodemailer = require('nodemailer')
 const cors = require('cors')
-const { sendOTPController, verifyOTPController, getAllMoklet, getAllTransaksiKurir, addTransaksiKurir, updateTransaksiKurir, updateTransaksiKurirStatus } = require('../controllers/transaksiKurir_controller')
+const transaksiKurirController = require('../controllers/transaksiKurir_controller')
 
 router.use(express.json());
 router.use(cors());
 //endpoint
-router.get("/get", getAllTransaksiKurir)
-router.post("/add", addTransaksiKurir)
-router.get("/getMoklet", getAllMoklet)
-// app.post("/find",transaksiKurirController.findTransaksiKurir)
-router.put("/update/:id", updateTransaksiKurir)
+router.get("/get", transaksiKurirController.getAllTransaksiKurir)
+router.post("/add", transaksiKurirController.addTransaksiKurir)
+router.get("/getMoklet", transaksiKurirController.getAllMoklet)
+// app.post("/find",transaksiKurirController.findTransaksiKurir
+router.post("/verify-OTP", transaksiKurirController.verifyOTP)
 // app.delete("/delete/:id", transaksiSiswaController.deleteTransaksiSiswa)
-router.post("/send-otp", sendOTPController)
-router.post("/verify-otp", verifyOTPController)
-router.post("/update-status", updateTransaksiKurirStatus)
+
 
 module.exports = router;
